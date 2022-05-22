50945722cdc7286c07c14f8b66d0a866183ee54e
nbryklia <skeletonpro1@mail.ru>
2022-05-22 15:31:43 +0500

    refactor for Move

--- src/Move.js
+++ src/Move.js
@@ -1,53 +1,57 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, ButtonGroup, Container, Form, FormGroup, Input, Label, Table} from 'reactstrap';
 import AppNavbar from './AppNavbar';
 import './App.css';
+import Calendar from "react-calendar";
 
-class Move extends Component {
+const emptyItem = {
+    moveId:0,
+    taskId:1,
+    taskMission: '',
+    senderId:1,
+    senderLastName: '',
+    senderName: '',
+    senderMiddleName: '',
+    recipientId:1,
+    recipientLastName: '',
+    recipientName: '',
+    recipientMiddleName: '',
+    moveStatusId:1,
+    moveStatusName: '',
+    moveDateStart: new Date().toISOString(),
+    moveDateEnd: new Date().toISOString(),
+    moveDesc: '',
+};
 
-    emptyItem = {
-        moveId:0,
-        taskId:1,
-        taskMission: '',
-        senderId:1,
-        senderLastName: '',
-        senderName: '',
-        senderMiddleName: '',
-        recipientId:1,
-        recipientLastName: '',
-        recipientName: '',
-        recipientMiddleName: '',
-        moveStatusId:1,
-        moveStatusName: '',
-        moveDateStart: new Date().toISOString(),
-        moveDateEnd: new Date().toISOString(),
-        moveDesc: '',
-    };
+export default function Move(){
 
-    constructor(props) {
-        super(props);
-        this.state = {moves: [], workers:[], moveStatuses:[], tasks:[], item: this.emptyItem, action: "get"};
-        this.remove = this.remove.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const [workers, setWorkers] = useState();
+    const [workerList, setWorkerList] = useState();
+    const [tasks, setTasks] = useState();
+    const [taskList, setTaskList] = useState();
+    const [moves, setMoves] = useState();
+    const [moveList, setMoveList] = useState();
+    const [moveStatuses, setMoveStatuses] = useState();
+    const [moveStatusList, setMoveStatusList] = useState();
+    const [item, setItem] = useState(emptyItem);
+    const [action, setAction] = useState("get" );
 
-    componentDidMount() {
-        fetch('http://localhost:8090/move/getlist')
-            .then(response => response.json())
-            .then(data => this.setState({moves: data}));
-        fetch('http://localhost:8090/worker/getlist')
-            .then(response => response.json())
-            .then(data => this.setState({workers: data}));
-        fetch('http://localhost:8090/movestatus/getlist')
-            .then(response => response.json())
-            .then(data => this.setState({moveStatuses: data}));
-        fetch('http://localhost:8090/task/getlist')
-            .then(response => response.json())
-            .then(data => this.setState({tasks: data}));
-    }
+    useEffect(() => {
+            fetch('http://localhost:8090/move/getlist')
+                .then(response => response.json())
+                .then(data => setMoves(data));
+            fetch('http://localhost:8090/worker/getlist')
+                .then(response => response.json())
+                .then(data => setWorkers(data));
+            fetch('http://localhost:8090/movestatus/getlist')
+                .then(response => response.json())
+                .then(data => setMoveStatuses(data));
+            fetch('http://localhost:8090/task/getlist')
+                .then(response => response.json())
+                .then(data => setTasks(data));
+        }, [action])
 
-    async remove(id) {
+    async function remove(id) {
         await fetch(`http://localhost:8090/move/delete`, {
             method: 'DELETE',
             headers: {
@@ -56,21 +60,20 @@ class Move extends Component {
             },
             body: JSON.stringify(id)
         });
-        window.location.reload();
+        setAction("delete")
     }
 
-    async change(id){
+    async function change(id){
         const move = await (await fetch(`http://localhost:8090/move/get`,{method: "POST", body: JSON.stringify(id)})).json();
-        this.setState({item: move, action: "change"});
+        setItem(move);
+        setAction("change");
     }
 
-    async add(){
-        this.setState({action: "add"});
+    async function add(){
+        setAction("add")
     }
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        let item = this.state.item;
+    async function handleSubmit(){
         await fetch('http://localhost:8090/move/update', {
             method: 'PUT',
             headers: {
@@ -79,147 +82,139 @@ class Move extends Component {
             },
             body: JSON.stringify(item),
         });
-        this.props.history.push('/move');
     }
 
-    handleChange(event) {
+    function handleChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        const item = this.state.item;
-        item[name] = value;
-        this.setState({item: item});
-    };
-
-    postChange(event){
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        const item = this.state.item;
-        item[name] = value;
-        this.setState({item: item});
+        const item1 = item;
+        item1[name] = value;
+        setItem(item1);
     }
 
-    render() {
-        const {moves, workers, moveStatuses, tasks, isLoading} = this.state;
+    function handleDateStartChange(value){
+        let item1 = item;
+        item1["moveDateStart"] = value
+        setItem(item1)
+    }
 
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
+    function handleDateEndChange(value){
+        let item1 = item;
+        item1["moveDateEnd"] = value
+        setItem(item1)
+    }
 
-        if(this.state.action === "get") {
-            const moveList = moves.map(move => {
-                return <tr key={move.moveId}>
-                    <td style={{whiteSpace: 'nowrap'}}>{move.taskMission}</td>
-                    <td>{move.senderLastName} {move.senderName} {move.senderMiddleName}</td>
-                    <td>{move.recipientLastName} {move.recipientName} {move.recipientMiddleName}</td>
-                    <td>{move.moveStatusName}</td>
-                    <td>{move.moveDateStart}</td>
-                    <td>{move.moveDateEnd}</td>
-                    <td>{move.moveDesc}</td>
-                    <td>
-                        <ButtonGroup>
-                            <Button size="sm" color="primary"
-                                    onClick={() => this.change(move.moveId)}>Редактировать</Button>
-                            <Button size="sm" id="delete-button"
-                                    onClick={() => this.remove(move.moveId)}>Удалить</Button>
-                        </ButtonGroup>
-                    </td>
-                </tr>
-            });
-            return (
-                <div>
-                    <AppNavbar/>
-                    <Container fluid>
-                        <div className="float-right">
-                            <Button color="success" onClick={()=>this.add()}>Добавить заказ</Button>
-                        </div>
-                        <h3>Движения</h3>
-                        <Table className="mt-4">
-                            <thead>
-                            <tr>
-                                <th width="15%">Задание</th>
-                                <th width="20%">Отправитель</th>
-                                <th width="20%">Получатель</th>
-                                <th width="10%">Статус</th>
-                                <th width="10%">Дата начала</th>
-                                <th width="10%">Дата начала</th>
-                                <th width="40%">Описание</th>
-                            </tr>
-                            </thead>
-                            <tbody>
+    function view() {
+        setMoveList(moves?.map(move => {
+            return <tr key={move.moveId}>
+                <td style={{whiteSpace: 'nowrap'}}>{move.taskMission}</td>
+                <td>{move.senderLastName} {move.senderName} {move.senderMiddleName}</td>
+                <td>{move.recipientLastName} {move.recipientName} {move.recipientMiddleName}</td>
+                <td>{move.moveStatusName}</td>
+                <td>{new Date(move.moveDateStart).toLocaleDateString()}</td>
+                <td>{new Date(move.moveDateEnd).toLocaleDateString()}</td>
+                <td>{move.moveDesc}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary"
+                                onClick={() => change(move.moveId)}>Редактировать</Button>
+                        <Button size="sm" id="delete-button"
+                                onClick={() => remove(move.moveId)}>Удалить</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        }));
+        setTaskList(tasks?.map(task => {
+            return <option value = {task.taskId}>{task.taskMission}</option>
+        }));
+        setWorkerList(workers?.map(worker => {
+            return <option value = {worker.workerId}>{worker.workerLastName} {worker.workerName} {worker.workerMiddleName}</option>
+        }));
+        setMoveStatusList(moveStatuses?.map(moveStatus => {
+            return <option value = {moveStatus.moveStatusId}>{moveStatus.moveStatusName}</option>
+        }));
+    }
+
+    if(action === "get" || action === "delete") {
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" onClick={()=>add()}>Добавить движение</Button>
+                        <Button color="warning" onClick={()=>view()}>Обновить</Button>
+                    </div>
+                    <h3>Движения</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="15%">Задание</th>
+                            <th width="20%">Отправитель</th>
+                            <th width="20%">Получатель</th>
+                            <th width="10%">Статус</th>
+                            <th width="10%">Дата начала</th>
+                            <th width="10%">Дата конца</th>
+                            <th width="40%">Описание</th>
+                        </tr>
+                        </thead>
+                        <tbody>
                             {moveList}
-                            </tbody>
-                        </Table>
-                    </Container>
-                </div>
-            );
-        }
-        if(this.state.action === "change" || this.state.action === "add"){
-            const {item} = this.state;
-            const taskList = tasks.map(task => {
-                return <option value = {task.taskId}>{task.taskMission}</option>
-            })
-            const workerList = workers.map(worker => {
-                return <option value = {worker.workerId}>{worker.workerLastName} {worker.workerName} {worker.workerMiddleName}</option>
-            })
-            const moveStatusList = moveStatuses.map(moveStatus => {
-                return <option value = {moveStatus.moveStatusId}>{moveStatus.moveStatusName}</option>
-            })
-            const title = <h2>Редактирование информации о Движении</h2>;
-            return (
-                <div>
-                    <AppNavbar/>
-                    <Container>
-                        {title}
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup>
-                                <Label for="taskId">Задание</Label>
-                                <select name="taskId" id="taskId" defaultValue={item.taskId || ''}
-                                        onChange={this.handleChange} autoComplete="taskId">{taskList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="senderId">Отправитель</Label>
-                                <select name="senderId" id="senderId" defaultValue={item.senderId || ''}
-                                        onChange={this.handleChange} autoComplete="senderId">{workerList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="recipientId">Получатель</Label>
-                                <select name="recipientId" id="recipientId" defaultValue={item.recipientId || ''}
-                                        onChange={this.handleChange} autoComplete="recipientId">{workerList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="moveStatusId">Статус</Label>
-                                <select name="moveStatusId" id="moveStatusId" defaultValue={item.moveStatusId || ''}
-                                        onChange={this.handleChange} autoComplete="moveStatusId">{moveStatusList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="moveDateStart">Дата начала</Label>
-                                <Input type="text" name="moveDateStart" id="moveDateStart"
-                                       defaultValue={item.moveDateStart || ''}
-                                       onChange={this.handleChange} autoComplete="moveDateStart"/>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="moveDateEnd">Дата конца</Label>
-                                <Input type="text" name="moveDateEnd" id="moveDateEnd"
-                                       defaultValue={item.moveDateEnd || ''}
-                                       onChange={this.handleChange} autoComplete="moveDateEnd"/>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="moveDesc">Описание</Label>
-                                <Input type="text" name="moveDesc" id="moveDesc"
-                                       defaultValue={item.moveDesc || ''}
-                                       onChange={this.handleChange} autoComplete="moveDesc"/>
-                            </FormGroup>
-                            <FormGroup>
-                                <Button color="primary" type="submit">Сохранить</Button>{' '}
-                                <Button color="secondary" onClick={() => window.location.reload()}>Назад</Button>
-                            </FormGroup>
-                        </Form>
-                    </Container>
-                </div>
-            );
-        }
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+    if(action === "change" || action === "add"){
+        const title = <h2>Редактирование информации о Движении</h2>;
+        return (
+            <div>
+                <AppNavbar/>
+                <Container>
+                    {title}
+                    <Form>
+                        <FormGroup>
+                            <Label for="taskId">Задание</Label>
+                            <select name="taskId" id="taskId" defaultValue={item.taskId || ''}
+                                    onChange={handleChange} autoComplete="taskId">{taskList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="senderId">Отправитель</Label>
+                            <select name="senderId" id="senderId" defaultValue={item.senderId || ''}
+                                    onChange={handleChange} autoComplete="senderId">{workerList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="recipientId">Получатель</Label>
+                            <select name="recipientId" id="recipientId" defaultValue={item.recipientId || ''}
+                                    onChange={handleChange} autoComplete="recipientId">{workerList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="moveStatusId">Статус</Label>
+                            <select name="moveStatusId" id="moveStatusId" defaultValue={item.moveStatusId || ''}
+                                    onChange={handleChange} autoComplete="moveStatusId">{moveStatusList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="moveDateStart">Дата начала</Label>
+                            <Calendar value={new Date(new Date(item.moveDateStart).getTime())} onChange={handleDateStartChange} returnValue={"start"}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="moveDateEnd">Дата конца</Label>
+                            <Calendar value={new Date(new Date(item.moveDateEnd).getTime())} onChange={handleDateEndChange} returnValue={"start"}/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="moveDesc">Описание</Label>
+                            <Input type="text" name="moveDesc" id="moveDesc"
+                                   defaultValue={item.moveDesc || ''}
+                                   onChange={handleChange} autoComplete="moveDesc"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Button color="primary" onClick={() => handleSubmit()}>Сохранить</Button>{' '}
+                            <Button color="secondary" onClick={() => setAction("get")}>Назад</Button>
+                        </FormGroup>
+                    </Form>
+                </Container>
+            </div>
+        );
     }
 }
-export default Move;
