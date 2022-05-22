fd7a3492a32badbd08cf2b5fff7a263131e22145
nbryklia <skeletonpro1@mail.ru>
2022-05-22 11:43:10 +0500

    refactor for MoveStatus

--- src/MoveStatus.js
+++ src/MoveStatus.js
@@ -1,29 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {Button, ButtonGroup, Container, Form, FormGroup, Input, Label, Table} from 'reactstrap';
 import AppNavbar from './AppNavbar';
 
-class MoveStatus extends Component {
+const emptyItem = {
+    moveStatusId:0,
+    moveStatusName: ''
+};
 
-    emptyItem = {
-        moveStatusId:0,
-        moveStatusName: ''
-    };
+export default function MoveStatus() {
 
-    constructor(props) {
-        super(props);
-        this.state = {moveStatuses: [], item: this.emptyItem, action: "get"};
-        this.remove = this.remove.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const [moveStatuses, setMoveStatuses] = useState();
+    const [moveStatusList, setMoveStatusList] = useState();
+    const [item, setItem] = useState(emptyItem);
+    const [action, setAction] = useState("get" );
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('http://localhost:8090/movestatus/getlist')
             .then(response => response.json())
-            .then(data => this.setState({moveStatuses: data}));
-    }
+            .then(data => setMoveStatuses(data));
+    }, [action])
 
-    async remove(id) {
+    async function remove(id) {
         await fetch(`http://localhost:8090/movestatus/delete`, {
             method: 'DELETE',
             headers: {
@@ -32,22 +29,19 @@ class MoveStatus extends Component {
             },
             body: JSON.stringify(id)
         });
-        window.location.reload();
     }
 
-    async change(id){
+    async function change(id){
         const moveStatus = await (await fetch(`http://localhost:8090/movestatus/get`,{method: "POST", body: JSON.stringify(id)})).json();
-        this.setState({item: moveStatus, action: "change"});
+        setAction("change");
+        setItem(moveStatus);
     }
 
-    async add(){
-        this.setState({action: "add"});
+    async function add(){
+        setAction("add");
     }
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        let item = this.state.item;
-
+    async function handleSubmit() {
         await fetch('http://localhost:8090/movestatus/update', {
             method: 'PUT',
             headers: {
@@ -56,85 +50,78 @@ class MoveStatus extends Component {
             },
             body: JSON.stringify(item),
         });
-        this.props.history.push('/movestatus');
     }
 
-    handleChange(event) {
+    function handleChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        const item = this.state.item;
-        item[name] = value;
-        this.setState({item: item});
-    };
+        const item1 = item;
+        item1[name] = value;
+        setItem(item1);
+    }
 
-    render() {
-        const {moveStatuses, isLoading} = this.state;
+    const view = () => {
+        setMoveStatusList(moveStatuses?.map(moveStatus => {
+            return <tr key={moveStatus.moveStatusId}>
+                <td style={{whiteSpace: 'nowrap'}}>{moveStatus.moveStatusName}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary" onClick={() => change(moveStatus.moveStatusId)}>Редактировать</Button>
+                        <Button size="sm" id="delete-button" onClick={() => remove(moveStatus.moveStatusId)}>Удалить</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        }));
+    }
 
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
-
-        if(this.state.action === "get") {
-            const moveStatusList = moveStatuses.map(moveStatus => {
-                return <tr key={moveStatus.moveStatusId}>
-                    <td style={{whiteSpace: 'nowrap'}}>{moveStatus.moveStatusName}</td>
-                    <td>
-                        <ButtonGroup>
-                            <Button size="sm" color="primary" onClick={() => this.change(moveStatus.moveStatusId)}>Редактировать</Button>
-                            <Button size="sm" id="delete-button" onClick={() => this.remove(moveStatus.moveStatusId)}>Удалить</Button>
-                        </ButtonGroup>
-                    </td>
-                </tr>
-            });
-            return (
-                <div>
-                    <AppNavbar/>
-                    <Container fluid>
-                        <div className="float-right">
-                            <Button color="success" onClick={()=>this.add()}>Добавить статус движения</Button>
-                        </div>
-                        <h3>Статусы движения</h3>
-                        <Table className="mt-4">
-                            <thead>
-                            <tr>
-                                <th width="60%">Наименование статуса движения</th>
-                                <th width="40%">Действие</th>
-                            </tr>
-                            </thead>
-                            <tbody>
+    if(action === "get") {
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" onClick={() => add()}>Добавить статус движения</Button>
+                        <Button color="warning" onClick={() => view()}>Обновить</Button>
+                    </div>
+                    <h3>Статусы движения</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="60%">Наименование статуса движения</th>
+                            <th width="40%">Действие</th>
+                        </tr>
+                        </thead>
+                        <tbody>
                             {moveStatusList}
-                            </tbody>
-                        </Table>
-                    </Container>
-                </div>
-            );
-        }
-        if(this.state.action === "change" || this.state.action === "add"){
-            const {item} = this.state;
-            let title;
-            if(this.state.action === "change") title = <h2>Редактирование информации о статусе движения</h2>;
-            if(this.state.action === "add") title = <h2>Добавление информации о статусе движения</h2>;
-            return (
-                <div>
-                    <AppNavbar/>
-                    <Container>
-                        {title}
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup>
-                                <Label for="moveStatusName">Наименование статуса движения</Label>
-                                <Input type="text" name="moveStatusName" id="moveStatusName" defaultValue={item.moveStatusName || ''}
-                                       onChange={this.handleChange} autoComplete="moveStatusName"/>
-                            </FormGroup>
-                            <FormGroup>
-                                <Button color="primary" type="submit">Сохранить</Button>{' '}
-                                <Button color="secondary" onClick={()=>window.location.reload()}>Назад</Button>
-                            </FormGroup>
-                        </Form>
-                    </Container>
-                </div>
-            );
-        }
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+    if(action === "change" || action === "add"){
+        let title;
+        if(action === "change") title = <h2>Редактирование информации о статусе движения</h2>;
+        if(action === "add") title = <h2>Добавление информации о статусе движения</h2>;
+        return (
+            <div>
+                <AppNavbar/>
+                <Container>
+                    {title}
+                    <Form>
+                        <FormGroup>
+                            <Label for="moveStatusName">Наименование статуса движения</Label>
+                            <Input type="text" name="moveStatusName" id="moveStatusName" defaultValue={item.moveStatusName || ''}
+                                   onChange={handleChange} autoComplete="moveStatusName"/>
+                        </FormGroup>
+                        <FormGroup>
+                            <Button color="primary" onClick={() => handleSubmit()}>Сохранить</Button>{' '}
+                            <Button color="secondary" onClick={()=>setAction("get")}>Назад</Button>
+                        </FormGroup>
+                    </Form>
+                </Container>
+            </div>
+        );
     }
 }
-export default MoveStatus;
