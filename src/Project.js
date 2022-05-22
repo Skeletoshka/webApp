f73ee9e783b41ff189ccbd1c69a0e0497d1604ba
nbryklia <skeletonpro1@mail.ru>
2022-05-22 14:00:19 +0500

    refactor for Project

--- src/Project.js
+++ src/Project.js
@@ -1,45 +1,48 @@
-import React, { Component } from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, ButtonGroup, Container, Form, FormGroup, Input, Label, Table} from 'reactstrap';
 import AppNavbar from './AppNavbar';
 import './App.css';
 
-class Project extends Component {
+const emptyItem = {
+    projectId:0,
+    orderId:1,
+    workerId:1,
+    workerLastName: '',
+    workerName: '',
+    workerMiddleName: '',
+    projectStatusId:1,
+    projectStatusName: '',
+};
 
-    emptyItem = {
-        projectId:0,
-        orderId:1,
-        workerId:1,
-        workerLastName: '',
-        workerName: '',
-        workerMiddleName: '',
-        projectStatusId:1,
-        projectStatusName: '',
-    };
+export default function Project(){
 
-    constructor(props) {
-        super(props);
-        this.state = {projects: [], workers:[], projectStatuses:[], orders:[], item: this.emptyItem, action: "get"};
-        this.remove = this.remove.bind(this);
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-    }
+    const [workers, setWorkers] = useState();
+    const [workerList, setWorkerList] = useState();
+    const [projects, setProjects] = useState();
+    const [projectList, setProjectList] = useState();
+    const [projectStatuses, setProjectsStatuses] = useState();
+    const [projectStatusList, setProjectStatusList] = useState();
+    const [orders, setOrders] = useState();
+    const [orderList, setOrderList] = useState();
+    const [item, setItem] = useState(emptyItem);
+    const [action, setAction] = useState("get" );
 
-    componentDidMount() {
+    useEffect(() => {
         fetch('http://localhost:8090/project/getlist')
             .then(response => response.json())
-            .then(data => this.setState({projects: data}));
+            .then(data => setProjects(data));
         fetch('http://localhost:8090/worker/getlist')
             .then(response => response.json())
-            .then(data => this.setState({workers: data}));
+            .then(data => setWorkers(data));
         fetch('http://localhost:8090/projectstatus/getlist')
             .then(response => response.json())
-            .then(data => this.setState({projectStatuses: data}));
+            .then(data => setProjectsStatuses(data));
         fetch('http://localhost:8090/order/getlist')
             .then(response => response.json())
-            .then(data => this.setState({orders: data}));
-    }
+            .then(data => setOrders(data));
+    }, [action])
 
-    async remove(id) {
+    async function remove(id) {
         await fetch(`http://localhost:8090/project/delete`, {
             method: 'DELETE',
             headers: {
@@ -48,21 +51,20 @@ class Project extends Component {
             },
             body: JSON.stringify(id)
         });
-        window.location.reload();
+        setAction("delete");
     }
 
-    async change(id){
+    async function change(id){
         const worker = await (await fetch(`http://localhost:8090/project/get`,{method: "POST", body: JSON.stringify(id)})).json();
-        this.setState({item: worker, action: "change"});
+        setItem(worker);
+        setAction("change");
     }
 
-    async add(){
-        this.setState({action: "add"});
+    async function add(){
+        setAction("add")
     }
 
-    async handleSubmit(event) {
-        event.preventDefault();
-        let item = this.state.item;
+    async function handleSubmit() {
         await fetch('http://localhost:8090/project/update', {
             method: 'PUT',
             headers: {
@@ -71,116 +73,100 @@ class Project extends Component {
             },
             body: JSON.stringify(item),
         });
-        this.props.history.push('/project');
     }
 
-    handleChange(event) {
+    function handleChange(event) {
         const target = event.target;
         const value = target.value;
         const name = target.name;
-        const item = this.state.item;
-        item[name] = value;
-        this.setState({item: item});
-    };
-
-    postChange(event){
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-        const item = this.state.item;
-        item[name] = value;
-        this.setState({item: item});
+        const item1 = item;
+        item1[name] = value;
+        setItem(item1);
     }
 
-    render() {
-        const {projects, workers, projectStatuses,orders, isLoading} = this.state;
+    function view(){
+        setProjectList(projects?.map(project => {
+            return <tr key={project.projectId}>
+                <td style={{whiteSpace: 'nowrap'}}>{project.orderId}</td>
+                <td>{project.workerLastName} {project.workerName} {project.workerMiddleName}</td>
+                <td>{project.projectStatusName}</td>
+                <td>
+                    <ButtonGroup>
+                        <Button size="sm" color="primary"
+                                onClick={() => change(project.projectId)}>Редактировать</Button>
+                        <Button size="sm" id="delete-button"
+                                onClick={() => remove(project.projectId)}>Удалить</Button>
+                    </ButtonGroup>
+                </td>
+            </tr>
+        }));
+        setWorkerList(workers?.map(worker => {
+            return <option value = {worker.workerId}>{worker.workerLastName} {worker.workerName} {worker.workerMiddleName}</option>
+        }));
+        setProjectStatusList(projectStatuses?.map(projectStatus => {
+            return <option value = {projectStatus.projectStatusId}>{projectStatus.projectStatusName}</option>
+        }));
+        setOrderList(orders?.map(order => {
+            return <option value={order.orderId}>{order.orderId}</option>
+        }));
+    }
 
-        if (isLoading) {
-            return <p>Loading...</p>;
-        }
-
-        if(this.state.action === "get") {
-            const projectList = projects.map(project => {
-                return <tr key={project.projectId}>
-                    <td style={{whiteSpace: 'nowrap'}}>{project.orderId}</td>
-                    <td>{project.workerLastName} {project.workerName} {project.workerMiddleName}</td>
-                    <td>{project.projectStatusName}</td>
-                    <td>
-                        <ButtonGroup>
-                            <Button size="sm" color="primary"
-                                    onClick={() => this.change(project.projectId)}>Редактировать</Button>
-                            <Button size="sm" id="delete-button"
-                                    onClick={() => this.remove(project.projectId)}>Удалить</Button>
-                        </ButtonGroup>
-                    </td>
-                </tr>
-            });
-            return (
-                <div>
-                    <AppNavbar/>
-                    <Container fluid>
-                        <div className="float-right">
-                            <Button color="success" onClick={()=>this.add()}>Добавить заказ</Button>
-                        </div>
-                        <h3>Проекты</h3>
-                        <Table className="mt-4">
-                            <thead>
-                            <tr>
-                                <th width="30%">Номер договора</th>
-                                <th width="40%">Руководитель проекта</th>
-                                <th width="30%">Статус проекта</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                {projectList}
-                            </tbody>
-                        </Table>
-                    </Container>
-                </div>
-            );
-        }
-        if(this.state.action === "change" || this.state.action === "add"){
-            const {item} = this.state;
-            const workerList = workers.map(worker => {
-                return <option value = {worker.workerId}>{worker.workerLastName} {worker.workerName} {worker.workerMiddleName}</option>
-            })
-            const projectStatusList = projectStatuses.map(projectStatus => {
-                return <option value = {projectStatus.projectStatusId}>{projectStatus.projectStatusName}</option>
-            })
-            const orderList = orders.map(order => {
-                return <option value={order.orderId}>{order.orderId}</option>
-            })
-            const title = <h2>Редактирование информации о проекте</h2>;
-            return (
-                <div>
-                    <AppNavbar/>
-                    <Container>
-                        {title}
-                        <Form onSubmit={this.handleSubmit}>
-                            <FormGroup>
-                                <Label for="orderId">Номер договора</Label>
-                                <select name="orderId" id="orderId" defaultValue={item.orderId || ''}
-                                        onChange={this.handleChange} autoComplete="orderId">{orderList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="workerId">Руководитель проекта</Label>
-                                <select name="workerId" id="workerId" defaultValue={item.workerId || ''}
-                                        onChange={this.handleChange} autoComplete="workerId">{workerList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Label for="projectStatusId">Статус проекта</Label>
-                                <select name="projectStatusId" id="projectStatusId" defaultValue={item.projectStatusId || ''}
-                                        onChange={this.handleChange} autoComplete="projectStatusId">{projectStatusList}</select>
-                            </FormGroup>
-                            <FormGroup>
-                                <Button color="primary" type="submit">Сохранить</Button>{' '}
-                                <Button color="secondary" onClick={() => window.location.reload()}>Назад</Button>
-                            </FormGroup>
-                        </Form>
-                    </Container>
-                </div>
-            );
-        }
+    if(action === "get" || action === "delete") {
+        return (
+            <div>
+                <AppNavbar/>
+                <Container fluid>
+                    <div className="float-right">
+                        <Button color="success" onClick={()=>add()}>Добавить заказ</Button>
+                        <Button color="warning" onClick={()=>view()}>Обновить</Button>
+                    </div>
+                    <h3>Проекты</h3>
+                    <Table className="mt-4">
+                        <thead>
+                        <tr>
+                            <th width="30%">Номер договора</th>
+                            <th width="40%">Руководитель проекта</th>
+                            <th width="30%">Статус проекта</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {projectList}
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    }
+    if(action === "change" || action === "add"){
+        const title = <h2>Редактирование информации о проекте</h2>;
+        return (
+            <div>
+                <AppNavbar/>
+                <Container>
+                    {title}
+                    <Form>
+                        <FormGroup>
+                            <Label for="orderId">Номер договора</Label>
+                            <select name="orderId" id="orderId" defaultValue={item.orderId || ''}
+                                    onChange={handleChange} autoComplete="orderId">{orderList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="workerId">Руководитель проекта</Label>
+                            <select name="workerId" id="workerId" defaultValue={item.workerId || ''}
+                                    onChange={handleChange} autoComplete="workerId">{workerList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Label for="projectStatusId">Статус проекта</Label>
+                            <select name="projectStatusId" id="projectStatusId" defaultValue={item.projectStatusId || ''}
+                                    onChange={handleChange} autoComplete="projectStatusId">{projectStatusList}</select>
+                        </FormGroup>
+                        <FormGroup>
+                            <Button color="primary" onClick={() => handleSubmit()}>Сохранить</Button>{' '}
+                            <Button color="secondary" onClick={() => setAction("get")}>Назад</Button>
+                        </FormGroup>
+                    </Form>
+                </Container>
+            </div>
+        );
     }
 }
-export default Project;
